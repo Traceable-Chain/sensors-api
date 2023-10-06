0c94e23759defda69540880160f8a902ccbe8dbe
nhussein11 <nicolashussein14@gmail.com>
2023-10-06 20:40:41 -0300

    fix: updating test to new endpoint logic

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,15 +1,13 @@
 import request from "supertest";
 import app from "../src/app";
-import { TEMPERATURES_MOCK_UP } from "../src/data/temperaturesMockUp";
+import { SENSORS_DATA_MOCK_UP } from "../src/data/sensorsDataMockUp";
 
 describe("GET /api/v1/temperature", () => {
   it("responds with a json message", (done) => {
-    const expectedTemperatures = Object.fromEntries(TEMPERATURES_MOCK_UP);
-
     request(app)
-      .get("/api/v1/temperature")
+      .get("/api/v1/sensors")
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      .expect(200, { temperatures: expectedTemperatures }, done);
+      .expect(200, { data: SENSORS_DATA_MOCK_UP }, done);
   });
 });
