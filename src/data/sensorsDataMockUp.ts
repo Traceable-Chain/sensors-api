20b65db89105347c75fff98c515e2bdf4a0c4393
nhussein11 <nicolashussein14@gmail.com>
2023-10-06 20:38:43 -0300

    add: sensors data mock up

---
+++ src/data/sensorsDataMockUp.ts
@@ -0,0 +1,102 @@
+export const SENSORS_DATA_MOCK_UP = [
+  {
+    sensorId: "sensor-1",
+    timestamp: "2021-01-01T00:00:00.000Z",
+    geoLocation: {
+      latitude: 51.5074,
+      longitude: 0.1278,
+    },
+    value: 10,
+    type: "temperature" as const,
+  },
+  {
+    sensorId: "sensor-2",
+    timestamp: "2021-01-02T12:30:00.000Z",
+    geoLocation: {
+      latitude: 40.7128,
+      longitude: -74.006,
+    },
+    value: 25,
+    type: "humidity" as const,
+  },
+  {
+    sensorId: "sensor-3",
+    timestamp: "2021-01-03T06:45:00.000Z",
+    geoLocation: {
+      latitude: 34.0522,
+      longitude: -118.2437,
+    },
+    value: 1000,
+    type: "pressure" as const,
+  },
+  {
+    sensorId: "sensor-4",
+    timestamp: "2021-01-15T23:59:59.000Z",
+    geoLocation: {
+      latitude: 28.7041,
+      longitude: -81.4637,
+    },
+    value: 15,
+    type: "temperature" as const,
+  },
+  {
+    sensorId: "sensor-5",
+    timestamp: "2021-02-05T08:15:00.000Z",
+    geoLocation: {
+      latitude: 51.5074,
+      longitude: 0.1278,
+    },
+    value: 12,
+    type: "humidity" as const,
+  },
+  {
+    sensorId: "sensor-6",
+    timestamp: "2021-02-18T16:30:00.000Z",
+    geoLocation: {
+      latitude: 40.7128,
+      longitude: -74.006,
+    },
+    value: 980,
+    type: "pressure" as const,
+  },
+  {
+    sensorId: "sensor-7",
+    timestamp: "2021-03-10T14:20:00.000Z",
+    geoLocation: {
+      latitude: 34.0522,
+      longitude: -118.2437,
+    },
+    value: 18,
+    type: "temperature" as const,
+  },
+  {
+    sensorId: "sensor-8",
+    timestamp: "2021-03-25T20:45:00.000Z",
+    geoLocation: {
+      latitude: 28.7041,
+      longitude: -81.4637,
+    },
+    value: 22,
+    type: "humidity" as const,
+  },
+  {
+    sensorId: "sensor-9",
+    timestamp: "2021-04-12T04:10:00.000Z",
+    geoLocation: {
+      latitude: 52.52,
+      longitude: 13.405,
+    },
+    value: 1020,
+    type: "pressure" as const,
+  },
+  {
+    sensorId: "sensor-10",
+    timestamp: "2021-04-28T12:55:00.000Z",
+    geoLocation: {
+      latitude: 48.8566,
+      longitude: 2.3522,
+    },
+    value: 14,
+    type: "temperature" as const,
+  },
+];
